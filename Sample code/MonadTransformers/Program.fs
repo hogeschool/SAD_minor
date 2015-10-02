c8c5a527dfe20d9a15fbc0f868c984e3a9b0821d
Giuseppe Maggiore <giuseppemag@gmail.com>
2015-10-02 11:15:24 +0200

    Added monadic samples.

---
+++ Sample code/MonadTransformers/Program.fs
@@ -0,0 +1,51 @@
+﻿type MaybeResult<'a> = Done of 'a | Error of string
+type StateMaybe<'s,'a> = 's -> MaybeResult<'a * 's>
+type MaybeBuilder() = 
+  member this.Bind (a : MaybeResult<'a>, b : 'a -> MaybeResult<'b>) =
+    match a with
+    | Done c -> b c
+    | Error e -> Error e
+  member this.Return (a : 'a) = Done a
+  member this.ReturnFrom (x) = x
+
+let opt = MaybeBuilder()
+
+let ret x = fun s -> Done(x,s)
+let (>>=) (p:StateMaybe<'s,'a>) (k:'a->StateMaybe<'s,'b>) : StateMaybe<'s,'b> =
+  fun s0 ->
+    opt{
+      let! x,s1 = p s0
+      return! k x s1
+    }
+type StateMaybeBuilder() =
+  member this.Return(x) = ret x
+  member this.Bind(p,k) = p >>= k
+  member this.ReturnFrom(x) = x
+let st = StateMaybeBuilder()
+
+let fail msg = fun s -> Error msg
+
+type Memory = { A : int; B : int }
+  with static member Zero = { A = 0; B = 0 }
+let getA : StateMaybe<Memory, int> = fun m -> opt{ return m.A, m }
+let getB : StateMaybe<Memory, int> = fun m -> opt{ return m.B, m }
+let setA (v:int) : StateMaybe<Memory, Unit> = fun m -> opt{ return (), { m with A = v } }
+let setB (v:int) : StateMaybe<Memory, Unit> = fun m -> opt{ return (), { m with B = v } }
+
+let program =
+  st{
+    let! a = getA
+    let! b = getB
+    if b = 0 then 
+      return! fail "Cannot divide by zero"
+    else
+      do! setA (a/b)
+  }
+
+type StateMaybeList<'s,'a> = 's -> MaybeResult<List<'a> * 's>
+type ListStateMaybe<'s,'a> = List<'s> -> MaybeResult<List<'a * 's>>
+
+[<EntryPoint>]
+let main argv = 
+  printfn "%A" (program { A = 100; B = 0 })
+  0
---
+++ Sample code/StateMonad/Program.fs
@@ -0,0 +1,57 @@
+﻿type State<'s,'a> = 's -> 'a * 's
+
+let ret x = fun s -> x,s
+let (>>=) (p:State<'s,'a>) (k:'a->State<'s,'b>) : State<'s,'b> =
+  fun s0 ->
+    let x,s1 = p s0
+    k x s1
+
+type StateBuilder() =
+  member this.Return x = ret x
+  member this.Bind(p,k) = p >>= k
+  member this.Zero() = fun s -> (),s
+let st = StateBuilder()
+
+////
+
+type Memory = { A : int; B : int }
+  with static member Zero = { A = 0; B = 0 }
+let getA : State<Memory, int> = fun m -> m.A, m
+let getB : State<Memory, int> = fun m -> m.B, m
+let setA (v:int) : State<Memory, Unit> = fun m -> (), { m with A = v }
+let setB (v:int) : State<Memory, Unit> = fun m -> (), { m with B = v }
+
+let inline (!) ((<+>):'a->'b->'c) =
+  fun (p1:State<'s,'a>) (p2:State<'s,'b>) ->
+     st{
+      let! x = p1
+      let! y = p2
+      return x <+> y
+     }
+
+let (.&) (p1:State<'s,'a>) (p2:State<'s,'b>) : State<'s,'a * 'b> =
+  st{
+    let! x = p1
+    let! y = p2
+    return x,y
+  }
+
+type Either<'a,'b> = First of 'a | Second of 'b
+let (.|) (p1:State<'s,'a>) (p2:State<'s,'b>) (choice:('a*'s) -> ('b*'s) -> Either<'a,'b>*'s) : State<'s,Either<'a,'b>> =
+  fun s0 ->
+    let x,s1 = p1 s0
+    let y,s1' = p2 s0
+    choice (x,s1) (y,s1')
+
+let program =
+  st{
+    let! a,b = getA .& getB
+    do! setA (a+b+1)
+    do! setB (a*b+2)
+    return "I am done!"
+  }
+
+[<EntryPoint>]
+let main argv = 
+  printfn "%A" (program Memory.Zero)
+  0
